b2457514de3a3c806b2c6125ec663491000acd18
Melopeus <themelopeus@gmail.com>
2020-07-18 08:37:34 +0300

    hint functionality

--- public/paginateResults.js
+++ public/paginateResults.js
@@ -47,10 +47,6 @@ function practicePaginatedResults(model) {
         const startIndex = (page - 1) * limit
         const endIndex = page * limit
 
-        if(page != req.session.lastAccessed){
-            req.session.lastAccessed = page
-        }
-
         let response = {}
 
         let wordQuery = {}
@@ -63,6 +59,10 @@ function practicePaginatedResults(model) {
         wordQuery.ownerId = req.user._id.toString()
         const results = {}
         if (endIndex < await model.countDocuments(wordQuery).exec()) {
+            
+            if(page > req.session.lastAccessed){
+                req.session.lastAccessed = page
+            }
             results.next = {
                 page: page + 1,
                 limit: limit
@@ -78,6 +78,7 @@ function practicePaginatedResults(model) {
         try {
             results.results = await model.find(wordQuery).limit(limit).skip(startIndex).exec()
             res.paginatedResults = results
+            res.paginatedResults.lastAccessed = req.session.lastAccessed
             next()
         } catch (e) {
             res.status(500).json({ message: e.message })
